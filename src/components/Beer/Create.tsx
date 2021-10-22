01c7d308811eba75a7a3c96b2e75cfecf27d1f71
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2021-10-22 11:28:01 -0300

    primeiro projeto em react utilizando como base o Empório da cerveja

---
+++ src/components/Beer/Create.tsx
@@ -0,0 +1,36 @@
+import { FormEvent, useState } from "react";
+
+interface Beer {
+  id?: number,
+  name: string
+}
+
+export function Create() {
+  const [beer, setBeer] = useState<Beer>({} as Beer);
+
+  const saveBeer = async (ev: FormEvent<HTMLFormElement>) => {
+    if (beer.name.length < 3) {
+      alert('digite ao menos 3 caracteres');
+      return;
+    }
+
+    ev.preventDefault();
+
+    await fetch('http://localhost:8080/beers', {
+      method: 'POST', // usado para criar um novo registro (padrão REST)
+      headers: {
+        'Content-Type': 'application/json' // define que o dado enviado é um json
+      },
+      body: JSON.stringify(beer) // transformando um objeto em texto-json
+    });
+  }
+
+  return (
+    <form onSubmit={saveBeer}>
+      <input type="text" required minLength={3} onChange={(ev) => {
+        setBeer({ name: ev.target.value });
+      }} />
+      <button type="submit">Salvar</button>
+    </form>
+  );
+}
